14c8db6e50796dbce7be5f92ed131008bbf24cf5
nakt <nakt_azdim@walkure.net>
2020-03-14 17:48:24 +0900

    [add] とりあえず難度推定が回る状態になった

---
+++ irtorch/grm/estimate.py
@@ -0,0 +1,90 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import pytorch_lightning as pl
+
+from irtorch.grm.converter import GRMDataConverter
+from irtorch.grm.map_module import GRMMAPModule
+
+
+class GRMEstimator(pl.LightningModule):
+    def __init__(self,
+                 response_df: pd.DataFrame,
+                 a_prior_df: pd.DataFrame = None,
+                 b_prior_df: pd.DataFrame = None,
+                 t_prior_df: pd.DataFrame = None):
+        super(GRMEstimator, self).__init__()
+
+        self.converter = GRMDataConverter(response_df)
+
+        self.model = GRMMAPModule(
+            a_prior=self.converter.make_a_prior(a_prior_df),
+            b_prior=self.converter.make_b_prior(b_prior_df),
+            t_prior=self.converter.make_t_prior(t_prior_df),
+            num_responses_total=len(response_df)
+        )
+
+        indices = np.c_[self.converter.make_item_array(),
+                        self.converter.make_person_array(),
+                        self.converter.make_response_array()]
+        self.dataset = TensorDataset(torch.tensor(indices).long())
+
+        self.loss_total = 0.0
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters())
+
+    def forward(self, indices):
+        return self.model.forward(indices)
+
+    def training_step(self, batch, batch_idx):
+        loss = self.forward(*batch)
+        self.loss_total += loss
+        return {"loss": loss}
+
+    def on_epoch_end(self):
+        self.loss_total = 0.0
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset, batch_size=1000, shuffle=True)
+
+    def validation_step(self):
+        pass  # dummy implementation to enable validation
+
+    def validation_epoch_end(self, _):
+        return {
+            "val_loss": self.loss_total,
+            "log": {"log_posterior": -self.loss_total}
+        }
+
+    def output_results(self, dir_path: str):
+        dir_path = Path(dir_path)
+        dir_path.mkdir(parents=True, exist_ok=True)
+        self.converter.make_a_df(self.model.a.detach().numpy()).to_csv(dir_path / "a.csv", index=False)
+        self.converter.make_b_df(self.model.b.detach().numpy()).to_csv(dir_path / "b.csv", index=False)
+        self.converter.make_t_df(self.model.t.detach().numpy()).to_csv(dir_path / "t.csv", index=False)
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("response", type=str)
+    p.add_argument("--a-prior", type=str)
+    p.add_argument("--b-prior", type=str)
+    p.add_argument("--t-prior", type=str)
+    p.add_argument("-o", "--out-dir", type=str)
+    args = p.parse_args()
+
+    response_df = pd.read_csv(args.response)
+    estimator = GRMEstimator(response_df)
+
+    trainer = pl.Trainer(default_save_path=args.out_dir, max_epochs=1)
+    trainer.fit(estimator)
+    estimator.output_results(args.out_dir)
+
+
+if __name__ == "__main__":
+    main()
---
+++ irtorch/grm/map_module.py
@@ -0,0 +1,76 @@
+import numpy as np
+import torch
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .prior import GaussianPrior
+from .likelihood import log_likelihood2
+
+
+def log_normal(parameter: torch.Tensor, prior: GaussianPrior) -> Tensor:
+    # 正規化項 (1/√(2π)σ) は推定に影響しないので省略してある
+    prior_mean = torch.from_numpy(prior.mean).float()
+    prior_std = torch.from_numpy(prior.std).float()
+    return -(torch.sum(((parameter - prior_mean) / prior_std) ** 2)) / 2
+
+
+def positive(tensor: Tensor, eps=1.0e-5) -> Tensor:
+    return F.softplus(tensor) + eps  # ReLU だとうまくいかない (おそらく勾配消失のせい)
+
+
+class GRMMAPModule(nn.Module):
+    """
+    Graded Response Model のパラメータを MAP 推定するための Module
+    """
+
+    def __init__(self,
+                 a_prior: GaussianPrior, b_prior: GaussianPrior, t_prior: GaussianPrior,
+                 num_responses_total: int):
+        super().__init__()
+
+        # パラメータ、初期値はすべて Prior の mean
+        def p(arr: np.ndarray) -> nn.Parameter:
+            return nn.Parameter(torch.from_numpy(arr).float())
+        self.a_ = p(a_prior.mean)
+        self.b_base_ = p(b_prior.mean[:, 0:1])
+        self.b_diff_ = p(np.diff(b_prior.mean, axis=1))
+        self.t_ = p(t_prior.mean)
+
+        # その他
+        self.a_prior = a_prior
+        self.b_prior = b_prior
+        self.t_prior = t_prior
+        self.num_responses_total = num_responses_total
+
+    @property
+    def a(self) -> Tensor:
+        return positive(self.a_)
+
+    @property
+    def b(self):
+        return torch.cumsum(
+            torch.cat([self.b_base_, positive(self.b_diff_)], dim=1),
+            dim=1
+        )
+
+    @property
+    def t(self):
+        return self.t_
+
+    def forward(self, indices: Tensor) -> Tensor:
+        """
+
+        :param indices: item, person, response [shape=(n_responses, 3)]
+        :return:
+        """
+        a, b, t = self.a, self.b, self.t
+        item_index, person_index, response_index = indices[:, 0], indices[:, 1], indices[:, 2]
+
+        log_prior = log_normal(a, self.a_prior) + log_normal(b, self.b_prior) + log_normal(t, self.t_prior)
+        log_likelihood = log_likelihood2(a, b, t, item_index, person_index, response_index)
+
+        # SGDでデータの一部を渡すことを想定してpriorに補正をかけている
+        log_posterior = log_likelihood + log_prior * (indices.shape[0] / self.num_responses_total)
+
+        return -log_posterior  # 「事後確率の対数のマイナスを最小化」⇔「事後確率を最大化」
