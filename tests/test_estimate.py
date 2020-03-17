ce3adeacf6751c5b0d46c10cf950b95dbbe8bfc5
nakt <nakt_azdim@walkure.net>
2020-03-17 16:38:51 +0900

    [add] 推定処理が一通りこけずに動くことを確認するテストを書いた

---
+++ tests/test_estimate.py
@@ -0,0 +1,42 @@
+import tempfile
+
+import pytest
+import pandas as pd
+
+from irtorch.estimate import estimate
+from tests.util import df
+
+
+@pytest.fixture()
+def response_df() -> pd.DataFrame:
+    return df("input", "response.csv")
+
+
+@pytest.fixture()
+def level_df() -> pd.DataFrame:
+    return df("input", "level.csv")
+
+
+def test_estimate_nonhierarchical(response_df):
+    # とりあえずこけずに動くことだけ確認する
+    with tempfile.TemporaryDirectory() as temp_dir:
+        estimate(
+            response_df,
+            n_iter=2,
+            batch_size=1,
+            out_dir=temp_dir,
+            log_dir=temp_dir,
+        )
+
+
+def test_estimate_hierarchical(response_df, level_df):
+    # とりあえずこけずに動くことだけ確認する
+    with tempfile.TemporaryDirectory() as temp_dir:
+        estimate(
+            response_df,
+            n_iter=2,
+            batch_size=1,
+            out_dir=temp_dir,
+            log_dir=temp_dir,
+            level_df=level_df
+        )
