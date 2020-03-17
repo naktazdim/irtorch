850a1bf7eaba56a529b7ad99a30c11728ed9540b
nakt <nakt_azdim@walkure.net>
2020-03-17 21:20:32 +0900

    [refactor] converterから、出力変換に必要なデータの集合をクラスとして切り出し

--- irtorch/converter.py
+++ irtorch/converter.py
@@ -1,7 +1,29 @@
 from itertools import product
+from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
 import pandas as pd
+
+
+@dataclass()
+class GRMMeta:
+    item_category: pd.Categorical
+    person_category: pd.Categorical
+    n_grades: int
+    level_category: Optional[pd.Categorical] = None
+
+    @property
+    def n_items(self) -> int:
+        return len(self.item_category.categories)
+
+    @property
+    def n_persons(self) -> int:
+        return len(self.person_category.categories)
+
+    @property
+    def n_levels(self) -> Optional[int]:
+        return None if self.level_category is None else len(self.level_category.categories)
 
 
 class GRMDataConverter(object):
@@ -21,14 +43,11 @@ class GRMDataConverter(object):
         assert response_df.response.min() >= 1
 
         self.response_df = response_df.astype({"item": "category", "person": "category"})
-
-        self.item_category = self.response_df.item.dtype
-        self.person_category = self.response_df.person.dtype
-
-        self.n_items = len(self.item_category.categories)
-        self.n_persons = len(self.person_category.categories)
-        self.n_responses = len(response_df)
-        self.n_grades = response_df.response.max()
+        self.meta = GRMMeta(
+            self.response_df.item.dtype,
+            self.response_df.person.dtype,
+            response_df.response.max()
+        )
 
         if level_df is None:
             self.is_hierarchical = False
@@ -40,15 +59,14 @@ class GRMDataConverter(object):
             assert level_df.item.unique().all()
 
             self.level_df = pd.merge(
-                self.item_category.categories.to_frame(name="item"),
+                self.meta.item_category.categories.to_frame(name="item"),
                 level_df
                     .drop_duplicates(subset="item")
-                    .astype({"item": self.item_category}),
+                    .astype({"item": self.meta.item_category}),
                 how="left"
             )
             self.level_df["level"] = self.level_df.level.fillna("_unknown").astype({"level": "category"})
-            self.level_category = self.level_df.level.dtype
-            self.n_levels = len(self.level_category.categories)
+            self.meta.level_category = self.level_df.level.dtype
 
     def make_response_array(self) -> np.ndarray:
         """
@@ -74,8 +92,8 @@ class GRMDataConverter(object):
         :param a_array: shape=(n_items,)
         :return: columns=(item, a)
         """
-        assert a_array.shape == (self.n_items,)
-        return pd.DataFrame().assign(item=self.item_category.categories, a=a_array)
+        assert a_array.shape == (self.meta.n_items,)
+        return pd.DataFrame().assign(item=self.meta.item_category.categories, a=a_array)
 
     def make_b_df(self, b_array: np.ndarray) -> pd.DataFrame:
         """
@@ -91,10 +109,10 @@ class GRMDataConverter(object):
         :param b_array: shape=(n_items, n_grades - 1)
         :return: columns=(item, grade, b)
         """
-        assert b_array.shape == (self.n_items, self.n_grades - 1)
+        assert b_array.shape == (self.meta.n_items, self.meta.n_grades - 1)
         return pd.DataFrame(
-            product(self.item_category.categories,
-                    np.arange(2, self.n_grades + 1)),
+            product(self.meta.item_category.categories,
+                    np.arange(2, self.meta.n_grades + 1)),
             columns=["item", "grade"])\
             .assign(b=b_array.flatten())
 
@@ -104,8 +122,8 @@ class GRMDataConverter(object):
         :param t_array: shape=(n_persons,)
         :return: columns=(person, t)
         """
-        assert t_array.shape == (self.n_persons,)
-        return pd.DataFrame().assign(person=self.person_category.categories, t=t_array)
+        assert t_array.shape == (self.meta.n_persons,)
+        return pd.DataFrame().assign(person=self.meta.person_category.categories, t=t_array)
 
     def make_level_df(self, level_mean_array: np.ndarray, level_std_array: np.ndarray) -> pd.DataFrame:
         """
@@ -122,11 +140,11 @@ class GRMDataConverter(object):
         :param level_std_array: shape=(n_levels, n_grades - 1)
         :return: columns=(level, grade, mean, std)
         """
-        assert level_mean_array.shape == (self.n_levels, self.n_grades - 1)
-        assert level_std_array.shape == (self.n_levels, self.n_grades - 1)
+        assert level_mean_array.shape == (self.meta.n_levels, self.meta.n_grades - 1)
+        assert level_std_array.shape == (self.meta.n_levels, self.meta.n_grades - 1)
         return pd.DataFrame(
-            product(self.level_category.categories,
-                    np.arange(2, self.n_grades + 1)),
+            product(self.meta.level_category.categories,
+                    np.arange(2, self.meta.n_grades + 1)),
             columns=["item", "grade"]) \
             .assign(mean=level_mean_array.flatten(),
                     std=level_std_array.flatten())
