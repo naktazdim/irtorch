3838496e109cb3a096a84c83461c4c1d6c4080d7
nakt <nakt@walkure.net>
2020-05-06 13:08:33 +0900

    [add] setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='irtorch',
+    version='1.1',
+    description='Item Response Theory Parameter Estimator',
+    author='nakt',
+    author_email='nakt@walkure.net',
+    install_requires=['numpy', 'pandas', 'torch', 'pytorch_lightning'],
+    python_requires='>=3.7',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
+)
