7522047cedb462c0eec6988f91a0654b368a1b7b
nakt <nakt@walkure.net>
2021-03-05 23:18:36 +0900

    [refactor] to_csvs() の処理をDataFrameの生成とファイル出力とに分割し、DataFrameの生成のほうのテストを書いた

--- irtorch/estimate/converter/outputs.py
+++ irtorch/estimate/converter/outputs.py
@@ -1,5 +1,7 @@
 from itertools import product
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -65,7 +67,24 @@ def make_level_df(outputs: GRMOutputs, meta: GRMMeta) -> pd.DataFrame:
                 std=outputs.level_std_array.flatten())
 
 
-def to_csvs(outputs: GRMOutputs, dir_path: str, meta: GRMMeta):
+@dataclass()
+class OutputDFs:
+    a: pd.DataFrame
+    b: pd.DataFrame
+    t: pd.DataFrame
+    level: Optional[pd.DataFrame] = None
+
+    def to_csvs(self, dir_path: str):
+        dir_path = Path(dir_path)
+        dir_path.mkdir(parents=True, exist_ok=True)
+        self.a.to_csv(dir_path / "a.csv", index=False)
+        self.b.to_csv(dir_path / "b.csv", index=False)
+        self.t.to_csv(dir_path / "t.csv", index=False)
+        if self.level is not None:
+            self.level.to_csv(dir_path / "b_prior.csv", index=False)
+
+
+def make_output_dfs(outputs: GRMOutputs, meta: GRMMeta) -> OutputDFs:
     assert outputs.a_array.shape == (meta.n_items,)
     assert outputs.b_array.shape == (meta.n_items, meta.n_grades - 1)
     assert outputs.t_array.shape == (meta.n_persons,)
@@ -73,11 +92,15 @@ def to_csvs(outputs: GRMOutputs, dir_path: str, meta: GRMMeta):
         assert outputs.level_mean_array.shape == (meta.n_levels, meta.n_grades - 1)
         assert outputs.level_std_array.shape == (meta.n_levels, meta.n_grades - 1)
 
-    dir_path = Path(dir_path)
-    dir_path.mkdir(parents=True, exist_ok=True)
-
-    make_a_df(outputs, meta).to_csv(dir_path / "a.csv", index=False)
-    make_b_df(outputs, meta).to_csv(dir_path / "b.csv", index=False)
-    make_t_df(outputs, meta).to_csv(dir_path / "t.csv", index=False)
+    output_dfs = OutputDFs(
+        make_a_df(outputs, meta),
+        make_b_df(outputs, meta),
+        make_t_df(outputs, meta)
+    )
     if outputs.level_mean_array is not None:
-        make_level_df(outputs, meta).to_csv(dir_path / "b_prior.csv", index=False)
+        output_dfs.level = make_level_df(outputs, meta)
+    return output_dfs
+
+
+def to_csvs(outputs: GRMOutputs, dir_path: str, meta: GRMMeta):
+    make_output_dfs(outputs, meta).to_csvs(dir_path)
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from irtorch.estimate.converter import inputs_from_df, GRMMeta
 from irtorch.estimate.model import GRMInputs, GRMOutputs
-from irtorch.estimate.converter.outputs import make_a_df, make_b_df, make_t_df, to_csvs
+from irtorch.estimate.converter.outputs import make_a_df, make_b_df, make_t_df, make_output_dfs
 from tests.util import df, array
 
 
@@ -57,6 +57,29 @@ def test_make_t_df(meta_and_inputs, outputs):
     meta, _ = meta_and_inputs
     pd.testing.assert_frame_equal(
         make_t_df(outputs, meta),
+        df("output", "t.csv"),
+        check_dtype=False,
+        check_categorical=False
+    )
+
+
+def test_make_output_dfs(meta_and_inputs, outputs):
+    meta, _ = meta_and_inputs
+    output_dfs = make_output_dfs(outputs, meta)
+    pd.testing.assert_frame_equal(
+        output_dfs.a,
+        df("output", "a.csv"),
+        check_dtype=False,
+        check_categorical=False
+    )
+    pd.testing.assert_frame_equal(
+        output_dfs.b,
+        df("output", "b.csv"),
+        check_dtype=False,
+        check_categorical=False
+    )
+    pd.testing.assert_frame_equal(
+        output_dfs.t,
         df("output", "t.csv"),
         check_dtype=False,
         check_categorical=False
