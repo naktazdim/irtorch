61ec0c69b1cc369b6a7177fb83d1a3ba3eea3c1f
nakt <nakt@walkure.net>
2021-03-10 00:03:16 +0900

    [fix] 入出力系のテストが壊れていたので修正

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from irtorch.estimate.converter import inputs_from_df, GRMMeta, InputDFs
 from irtorch.estimate.model import GRMInputs, GRMOutputs
-from irtorch.estimate.converter.outputs import make_a_df, make_b_df, make_t_df, make_output_dfs
+from irtorch.estimate.converter.outputs import make_output_dfs
 from tests.util import df, array
 
 
@@ -17,55 +17,24 @@ def meta_and_inputs() -> Tuple[GRMMeta, GRMInputs]:
     return inputs_from_df(input_dfs)
 
 
-@pytest.fixture()
-def outputs() -> GRMOutputs:
-    return GRMOutputs(
-        array("input", "a_array.csv"),
-        array("input", "b_array.csv"),
-        array("input", "t_array.csv")
-    )
-
-
 def test_make_response_array(meta_and_inputs):
     _, inputs = meta_and_inputs
     np.testing.assert_array_equal(
         inputs.response_array,
-        array("output", "response_array.csv", dtype=int)
+        array("array", "response_array.csv", dtype=int)
     )
 
 
-def test_make_a_df(meta_and_inputs, outputs):
+def test_make_output_dfs(meta_and_inputs):
     meta, _ = meta_and_inputs
-    pd.testing.assert_frame_equal(
-        make_a_df(outputs, meta),
-        df("output", "a.csv"),
-        check_dtype=False,
-        check_categorical=False
+    outputs = GRMOutputs(
+        array("array", "a_array.csv"),
+        array("array", "b_array.csv"),
+        array("array", "t_array.csv"),
+        array("array", "level_mean_array.csv"),
+        array("array", "level_mean_array.csv")
     )
 
-
-def test_make_b_df(meta_and_inputs, outputs):
-    meta, _ = meta_and_inputs
-    pd.testing.assert_frame_equal(
-        make_b_df(outputs, meta),
-        df("output", "b.csv"),
-        check_dtype=False,
-        check_categorical=False
-    )
-
-
-def test_make_t_df(meta_and_inputs, outputs):
-    meta, _ = meta_and_inputs
-    pd.testing.assert_frame_equal(
-        make_t_df(outputs, meta),
-        df("output", "t.csv"),
-        check_dtype=False,
-        check_categorical=False
-    )
-
-
-def test_make_output_dfs(meta_and_inputs, outputs):
-    meta, _ = meta_and_inputs
     output_dfs = make_output_dfs(outputs, meta)
     pd.testing.assert_frame_equal(
         output_dfs.a,
@@ -82,6 +51,12 @@ def test_make_output_dfs(meta_and_inputs, outputs):
     pd.testing.assert_frame_equal(
         output_dfs.t,
         df("output", "t.csv"),
+        check_dtype=False,
+        check_categorical=False
+    )
+    pd.testing.assert_frame_equal(
+        output_dfs.level,
+        df("output", "b_prior.csv"),
         check_dtype=False,
         check_categorical=False
     )
--- tests/test_estimate.py
+++ tests/test_estimate.py
@@ -1,30 +1,17 @@
 import tempfile
 
-import pytest
-import pandas as pd
-
 from irtorch.estimate import estimate
 from tests.util import df
 
 
-@pytest.fixture()
-def response_df() -> pd.DataFrame:
-    return df("input", "response.csv")
-
-
-@pytest.fixture()
-def level_df() -> pd.DataFrame:
-    return df("input", "level.csv")
-
-
-def test_estimate(response_df, level_df):
+def test_estimate():
     # とりあえずこけずに動くことだけ確認する
     with tempfile.TemporaryDirectory() as temp_dir:
         estimate(
-            response_df,
+            df("input", "response.csv"),
             n_iter=2,
             batch_size=1,
             out_dir=temp_dir,
             log_dir=temp_dir,
-            level_df=level_df
+            level_df=df("input", "level.csv")
         )
