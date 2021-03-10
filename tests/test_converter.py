7c0b2495722413995b16729715d002d235ad2df2
nakt <nakt@walkure.net>
2021-03-10 21:52:43 +0900

    [refactor] ConverterのテストをConverterを使うように修正 (?)

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,41 +1,34 @@
-from typing import Tuple
-
-import pytest
-
 import numpy as np
 import pandas as pd
 
-from irtorch.estimate.converter import inputs_from_df, GRMMeta, InputDFs
-from irtorch.estimate.model import GRMInputs, GRMOutputs
-from irtorch.estimate.converter.outputs import make_output_dfs
+from irtorch.estimate.converter import Converter
+from irtorch.estimate.entities import InputDFs
+from irtorch.estimate.model import GRMOutputs
 from tests.util import df, array
 
 
-@pytest.fixture()
-def meta_and_inputs() -> Tuple[GRMMeta, GRMInputs]:
-    input_dfs = InputDFs(df("input", "response.csv"), df("input", "level.csv"))
-    return inputs_from_df(input_dfs)
+def test_converter():
+    converter = Converter()
+    input_dfs = InputDFs(
+        df("input", "response.csv"),
+        df("input", "level.csv"),
+    )
+    grm_inputs = converter.inputs_from_dfs(input_dfs)
 
-
-def test_make_response_array(meta_and_inputs):
-    _, inputs = meta_and_inputs
     np.testing.assert_array_equal(
-        inputs.response_array,
-        array("array", "response_array.csv", dtype=int)
+        grm_inputs.response_array,
+        array("array", "response_array.csv")
     )
 
-
-def test_make_output_dfs(meta_and_inputs):
-    meta, _ = meta_and_inputs
-    outputs = GRMOutputs(
+    grm_outputs = GRMOutputs(
         array("array", "a_array.csv"),
         array("array", "b_array.csv"),
         array("array", "t_array.csv"),
         array("array", "level_mean_array.csv"),
         array("array", "level_mean_array.csv")
     )
+    output_dfs = converter.outputs_to_dfs(grm_outputs)
 
-    output_dfs = make_output_dfs(outputs, meta)
     pd.testing.assert_frame_equal(
         output_dfs.a,
         df("output", "a.csv"),
