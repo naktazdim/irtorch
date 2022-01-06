64c3454dd442d2db8bc69301eeb00dd4922c07e4
nakt <nakt@walkure.net>
2022-01-06 22:29:34 +0900

    [fix] setup.py 更新忘れ

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='irtorch',
-    version='1.1',
+    version='2.1',
     description='Item Response Theory Parameter Estimator',
     author='nakt',
     author_email='nakt@walkure.net',
-    install_requires=['numpy', 'pandas', 'torch', 'pytorch_lightning'],
+    install_requires=['numpy', 'pandas', 'torch', 'pytorch_lightning', 'fire'],
     python_requires='>=3.7',
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests'
